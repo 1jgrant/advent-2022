78108d3169e50b12658590865d8cdb02730b9972
1jgrant <grantja92@gmail.com>
2022-12-06 17:20:13 +0000

    updates day 6

--- src/day6.py
+++ src/day6.py
@@ -3,18 +3,13 @@ with open('src/inputs/day6_input.txt', 'r', encoding='utf-8') as text:
     actual_input = text.read()
 buffer = actual_input
 
-packet_marker_length = 4
-for i in range(packet_marker_length, len(buffer)):
-    block = buffer[i-packet_marker_length:i]
-    block_set = set(block)
-    if len(block_set) == packet_marker_length:
-        print('packet marker', i)
-        break
 
-message_marker_length = 14
-for i in range(message_marker_length, len(buffer)):
-    block = buffer[i-message_marker_length:i]
-    block_set = set(block)
-    if len(block_set) == message_marker_length:
-        print('message marker', i)
-        break
+def find_marker(buffer: str, marker_length: int) -> int:
+    for i in range(marker_length, len(buffer)):
+        block_set = set(buffer[i-marker_length:i])
+        if len(block_set) == marker_length:
+            return i
+
+
+print(find_marker(actual_input, 4))
+print(find_marker(actual_input, 14))
